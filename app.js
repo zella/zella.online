30de8b165e2dabdab230d13a72e6d146ce3a7864
zella <drumirage@gmail.com>
2019-02-17 03:39:54 +0300

    init

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const expressWs = require('express-ws')(app);
+const WebSocket = require('ws');
+const mustacheExpress = require('mustache-express');
+
+const PORT = parseInt(process.env.PORT) || 3000;
+const PROCAAS_WS_URL = process.env.PROCAAS_URL;
+const PTY_PROXY_CMD = JSON.parse(process.env.PTY_PROXY_CMD)
+
+app.use('/', express.static(__dirname));
+
+app.engine('html', mustacheExpress());
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.get('/run/:image', function (req, res) {
+    res.render('myxterm', {
+        image: req.params.image,
+    });
+});
+
+expressWs.app.ws('/docker', function (ws, req) {
+    const image = req.query.image;
+    const cols = req.query.cols;
+    const rows = req.query.rows;
+
+    //parameters to process( https://github.com/zella/procaas )
+    const processCmd = PTY_PROXY_CMD;
+    const processEnvs = {
+        'COLS': cols,
+        'ROWS': rows,
+        'SHELL': 'docker',
+        //env must be a string
+        //mem, cpu don't care, i think app will have only one user - me :)
+        'CMD': JSON.stringify(['run', '-it', '--rm', image])
+    };
+    const dataParam = encodeURIComponent(JSON.stringify({'cmd': processCmd, 'envs': processEnvs}));
+
+    const client = new WebSocket(PROCAAS_WS_URL + '?data=' + dataParam);
+
+    client.on('message', function incoming(data) {
+        ws.send(data);
+    });
+
+    ws.on('message', function (data) {
+        client.send(data)
+    });
+});
+
+app.listen(PORT);
